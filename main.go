94511b5d293b82c5b0812db67e3fd64ecaade22f
George Pugachev <39760024+GeorgeHub2018@users.noreply.github.com>
2019-01-19 11:19:00 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//Shape shape
+type Shape interface {
+	Square() float64
+	Perim() float64
+	Say()
+}
+
+// ShapeList shape list
+type ShapeList struct {
+	List map[string]Shape
+}
+
+// Rectangle struct with rectangle data
+type Rectangle struct {
+	Width  float64
+	Height float64
+	Type   string
+}
+
+// Triangle struct with triangle data
+type Triangle struct {
+	SideA float64
+	SideB float64
+	SideC float64
+	Type  string
+}
+
+// Circle struct with circle data
+type Circle struct {
+	Radius float64
+	Type   string
+}
+
+// Square method calculate square of the rectangle
+func (r Rectangle) Square() float64 {
+	return float64(r.Height * r.Width)
+}
+
+// Perim method calculate perimiter of the rectangle
+func (r Rectangle) Perim() float64 {
+	return float64(r.Width*2 + r.Height*2)
+}
+
+// Say method print all data of the rectangle
+func (r Rectangle) Say() {
+	fmt.Printf("%s Width: %v Height: %v Square: %v Perim: %v \n",
+		r.Type, r.Width, r.Height, r.Square(), r.Perim())
+}
+
+// Square method calculate square of the Triangle
+func (r Triangle) Square() float64 {
+	if (r.SideA > r.SideB) && (r.SideA > r.SideC) {
+		return float64(r.SideB * r.SideC / 2)
+	}
+
+	if (r.SideB > r.SideA) && (r.SideB > r.SideC) {
+		return float64(r.SideA * r.SideC / 2)
+	}
+
+	return float64(r.SideA * r.SideB / 2)
+}
+
+// Perim method calculate perimiter of the Triangle
+func (r Triangle) Perim() float64 {
+	return float64(r.SideA + r.SideB + r.SideC)
+}
+
+// Say method print all data of the Triangle
+func (r Triangle) Say() {
+	fmt.Printf("%s SideA: %v SideB: %v SideC: %v Square: %v Perim: %v \n",
+		r.Type, r.SideA, r.SideB, r.SideC, r.Square(), r.Perim())
+}
+
+// Square method calculate square of the Circle
+func (r Circle) Square() float64 {
+	return float64(r.Radius * r.Radius * math.Pi)
+}
+
+// Perim method calculate perimiter of the Circle
+func (r Circle) Perim() float64 {
+	return float64(r.Radius * 2 * math.Pi)
+}
+
+// Say method print all data of the Circle
+func (r Circle) Say() {
+	fmt.Printf("%s Radius:%v Square: %v Perim: %v \n",
+		r.Type, r.Radius, r.Square(), r.Perim())
+}
+
+func newRectangle(Width, Height float64) Rectangle {
+	return Rectangle{Width, Height, "Rectangle"}
+}
+func newTriangle(SideA, SideB, SideC float64) Triangle {
+	return Triangle{SideA, SideB, SideC, "Triangle"}
+}
+func newCircle(Radius float64) Circle {
+	return Circle{Radius, "Circle"}
+}
+func main() {
+	list := ShapeList{List: make(map[string]Shape)}
+
+	list.List["1"] = newCircle(5)
+	list.List["2"] = newRectangle(5, 6)
+	list.List["3"] = newTriangle(3, 4, 5)
+
+	for _, shape := range list.List {
+		shape.Say()
+	}
+}
